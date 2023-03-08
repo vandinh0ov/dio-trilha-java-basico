268319679ead5a6a3243dfaa289ef94177e141dc
givandilson.lima <givandilson.lima@MVRCSRV-N16660.mvrec.local>
2023-03-08 13:53:41 -0300

    Variaveis

--- src/edu/vando/tiposVariaveis/TiposVariaveis.java
+++ src/edu/vando/tiposVariaveis/TiposVariaveis.java
@@ -2,4 +2,17 @@ package edu.vando.tiposVariaveis;
 
 public class TiposVariaveis {
 
+	public static void main(String[] args) {
+		double salarioMinimo = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal= numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		int numero = 5;
+		numero = 10;
+		System.out.println(numero);
+		
+		final double VALOR_DE_PI = 3.14;
+	}
 }
